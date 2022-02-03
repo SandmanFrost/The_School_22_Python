13e9ff0b703980d92cc6cf57225917b1e83918b5
SandmanFrost <90185784+SandmanFrost@users.noreply.github.com>
2022-02-03 23:41:33 +0300

    Update Регулярные выражения.py

--- Регулярные выражения.py
+++ Регулярные выражения.py
@@ -115,3 +115,127 @@ if re.search(pattern, "1ab"):
     print("1ab +")
 else:
     print("1ab -")
+
+# Другие метасимволы (*, +, ?, { и })
+print("\nМетасимволы *, +, ?, { и } :")
+# * - означает 0 или более упоминаний объекта поиска
+pattern = r"egg(spam)*"
+if re.match(pattern, "egg"):
+    print("\negg +")
+else:
+    print("\negg -")
+if re.match(pattern, "eggspamspamegg"):
+    print("eggspamspamegg +")
+else:
+    print("eggspamspamegg -")
+if re.match(pattern, "spam"):
+    print("spam +")
+else:
+    print("spam -")
+# + - означает 1 или более упоминаний
+pattern = r"g+"
+if re.match(pattern, "g"):
+    print("\ng +")
+else:
+    print("\ng -")
+if re.match(pattern, "gggg"):
+    print("gggg +")
+else:
+    print("gggg -")
+if re.match(pattern, "span"):
+    print("span +")
+else:
+    print("span -")
+# ? - означает 0 или 1 повторение
+pattern = r"ice(-)?cream"
+if re.match(pattern, "ice-cream"):
+    print("\nice-cream +")
+else:
+    print("\nice-cream -")
+if re.match(pattern, "icecream"):
+    print("icecream +")
+else:
+    print("icecream -")
+if re.match(pattern, "ice--ice"):
+    print("ice--ice +")
+else:
+    print("ice--ice -")
+if re.match(pattern, "sousage"):
+    print("sousage +")
+else:
+    print("sousage -")
+if re.match(pattern, "ice--cream"):
+    print("ice--cream +")
+else:
+    print("ice--cream -")
+# {x,y} - означает диапозон поиска упоминаний между x и y
+pattern = r"9{1,3}$"
+if re.match(pattern, "9"):
+    print("\n9 +")
+else:
+    print("\n9 -")
+if re.match(pattern, "9989"):
+    print("9989 +")
+else:
+    print("9989 -")
+if re.match(pattern, "999"):
+    print("999 +")
+else:
+    print("999 -")
+# Выводимые данные:
+# -
+# +
+# ['spam', 'spam']
+# ggs
+# 1
+# 4
+# (1, 4)
+
+# Поиск и замена символа(-ов) в строке
+# Hello,-my-name-is-Joch.-Welcome-Joch.
+
+# Метасимволы:
+# Метасимвол . :
+# grey +
+# gray +
+# blue -
+
+# Метасимволы ^ и $ :
+# grey +
+# gray +
+# stringrey -
+
+# Классы символов:
+# grey +
+# qwertyuiop +
+# rhythm myths -
+
+# Поиск классов в диапозоне символов в строке([a-z],[A-Z],[0-9],[a-zA-Z])
+# LS8 +
+# E3FF1 +
+# 1ab -
+
+# Инвертация([^a-z],[^A-Z],[^0-9],[^a-zA-Z])
+# LS -
+# E3 +
+# 1ab +
+
+# Метасимволы *, +, ?, { и } :
+
+# egg +
+# eggspamspamegg +
+# spam -
+
+# g +
+# gggg +
+# span -
+
+# ice-cream +
+# icecream +
+# ice--ice -
+# sousage -
+# ice--cream -
+
+# 9 +
+# 9989 -
+# 999 -
